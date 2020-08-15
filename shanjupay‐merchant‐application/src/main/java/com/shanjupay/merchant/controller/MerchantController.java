ea807111863e3ab285d04c0cc2d15fd155ee311b
xsh <xsh2668028614@qq.com>
2020-08-16 00:19:45 +0800

    集成swagger-ui

---
+++ shanjupay‐merchant‐application/src/main/java/com/shanjupay/merchant/controller/MerchantController.java
@@ -0,0 +1,45 @@
+package com.shanjupay.merchant.controller;
+
+import com.shanjupay.merchant.api.MerchantService;
+import com.shanjupay.merchant.api.dto.MerchantDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : xsh
+ * @create : 2020-08-15 - 23:53
+ * @describe:
+ */
+@Api(value = "商户平台‐商户相关", tags = "商户平台‐商户相关", description = "商户平台‐商户相关")
+@RestController
+public class MerchantController {
+
+    @Reference
+    private MerchantService merchantService;
+
+    @ApiOperation("根据id查询商户")
+    @GetMapping("/merchants/{id}")
+    public MerchantDTO queryMerchantById(@PathVariable("id") Long id) {
+        MerchantDTO merchantDTO = merchantService.queryMerchantById(id);
+        return merchantDTO;
+    }
+
+    @ApiOperation("测试")
+    @GetMapping(path = "/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @ApiOperation("测试")
+    @ApiImplicitParam(name = "name", value = "姓名", required = true, dataType = "string")
+    @PostMapping(value = "/hi")
+    public String hi(String name) {
+        return "hi," + name;
+    }
+}
